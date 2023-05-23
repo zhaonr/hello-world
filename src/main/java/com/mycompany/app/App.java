8729479fa6e9921b9cb517da34426f33d88f1c90
zld <zhaoliangdi@sina.com>
2023-05-23 20:19:18 +0800

    first

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // test github action 2
+        System.out.println( "Hello World!2223" );
+    }
+}
